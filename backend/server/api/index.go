4190873ac52c41584579424b5f61d4a5e93769b0
Tom Draper <tomjdraper1@gmail.com>
2022-11-24 01:54:54 +0000

    Squash

---
+++ backend/server/api/index.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	supa "github.com/nedpals/supabase-go"
+)
+
+var (
+	app *gin.Engine
+)
+
+func getDBLogin() (string, string) {
+	supabaseURL := os.Getenv("SUPABASE_URL")
+	supabaseKey := os.Getenv("SUPABASE_KEY")
+	return supabaseURL, supabaseKey
+}
+
+type User struct {
+	UserID    string    `json:"user_id"`
+	APIKey    string    `json:"api_key"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func GenAPIKeyHandler(supabase *supa.Client) gin.HandlerFunc {
+	genAPIKey := func(c *gin.Context) {
+		var row struct{} // Insert empty row, use default values
+		var result []User
+		err := supabase.DB.From("Users").Insert(row).Execute(&result)
+		if err != nil {
+			panic(err)
+		}
+		apiKey := result[0].APIKey
+		c.JSON(200, gin.H{"status": 200, "api-key": apiKey})
+	}
+
+	return gin.HandlerFunc(genAPIKey)
+}
+
+type Request struct {
+	APIKey       string `json:"api_key"`
+	Path         string `json:"path"`
+	UserAgent    string `json:"user_agent"`
+	Method       int16  `json:"method"`
+	StatusCode   int16  `json:"status_code"`
+	ResponseTime int16  `json:"response_time"`
+}
+
+func LogRequestHandler(supabase *supa.Client) gin.HandlerFunc {
+	logRequest := func(c *gin.Context) {
+		var request Request
+		if err := c.BindJSON(&request); err != nil {
+			panic(err)
+		}
+
+		var result []interface{}
+		if err := supabase.DB.From("Requests").Insert(request).Execute(&result); err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "API request logged successfully."})
+	}
+
+	return gin.HandlerFunc(logRequest)
+}
+
+func registerRouter(r *gin.RouterGroup, supabase *supa.Client) {
+	r.GET("/gen-api-key", GenAPIKeyHandler(supabase))
+	r.POST("/request", LogRequestHandler(supabase))
+}
+
+func init() {
+	supabaseURL, supabaseKey := getDBLogin()
+	supabase := supa.CreateClient(supabaseURL, supabaseKey)
+
+	app = gin.New()
+
+	r := app.Group("/api") // Vercel - must be /api/xxx
+
+	registerRouter(r, supabase) // Register route
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
+}
