c2004261e01784fddfcb4b68a9d1f71f31d71b34
Tom Draper <tomjdraper1@gmail.com>
2022-11-25 17:31:16 +0000

    Past month error display

--- dashboard/src/components/PastMonth.svelte
+++ dashboard/src/components/PastMonth.svelte
@@ -7,7 +7,24 @@
     return date > monthAgo;
   }
 
+  let colors = [
+    'grey',
+    '#3FCF8E',
+    '#77D884',
+    '#A1DF7E',
+    '#C7E57D',
+    '#EBEB81',
+    '#EBEB81',
+    '#F3C966',
+    '#F5A65A',
+    '#F18359',
+    '#E46161'
+  ]
 
+  function daysAgo(date: Date): number {
+    let now = new Date()
+    return Math.floor((now.getTime() - date.getTime())/(24 * 60 * 60 * 1000))
+  }
 
   function build() {
     let successRate = {}
@@ -15,17 +32,34 @@
         let date = new Date(data[i].created_at);
         if (pastMonth(date)) {
             date.setHours(0, 0, 0, 0)
+            // @ts-ignore
             if (!(date in successRate)) {
+                // @ts-ignore
                 successRate[date] = {total: 0, successful: 0}
             }
             if (data[i].status >= 200 && data[i].status <= 299) {
+                // @ts-ignore
                 successRate[date].successful++
             }
+            // @ts-ignore
             successRate[date].total++
         }
     }
+
+    console.log(successRate)
+
+    let sr = new Array(60).fill(0)
+
+    for (let date in successRate) {
+        let idx = daysAgo(new Date(date))
+        console.log(date, idx)
+        sr[sr.length-idx] = (successRate[date].successful / successRate[date].total)
+    }
+    console.log(sr)
+    _successRate = sr;
   }
 
+  let _successRate
   onMount(() => {
     build();
   });
@@ -36,11 +70,13 @@
 <div class="card">
   <div class="card-title">Past Month</div>
 
-  <div class="errors">
-    {#each Array(60) as x, i}
-      <div class="error" />
-    {/each}
-  </div>
+  {#if _successRate != undefined}
+    <div class="errors">
+        {#each _successRate as value, i}
+        <div class="error" style="background: {colors[Math.floor(value*10)]}" title="{(value*100).toFixed(1)}%"/>
+        {/each}
+    </div>
+  {/if}
 </div>
 
 <style>
