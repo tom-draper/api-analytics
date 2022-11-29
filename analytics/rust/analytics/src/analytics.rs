54e28530f5ae7dcd3beb6639da231e76a9a18f62
Tom Draper <tomjdraper1@gmail.com>
2022-11-29 22:36:21 +0000

    Update toml file

--- analytics/rust/analytics/src/analytics.rs
+++ analytics/rust/analytics/src/analytics.rs
@@ -14,8 +14,8 @@ pub struct Analytics {
 }
 
 impl Analytics {
-    pub fn new(api_key: &str) -> Self {
-        Self { api_key: api_key.to_owned() }
+    pub fn new(api_key: String) -> Self {
+        Self { api_key }
     }
 }
 
