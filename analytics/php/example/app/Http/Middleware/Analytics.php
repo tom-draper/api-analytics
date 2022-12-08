6055f5a5eae451afc31479185282cd3c3c830b69
Tom Draper <tomjdraper1@gmail.com>
2022-12-08 21:54:25 +0000

    Fix bug

--- analytics/php/example/app/Http/Middleware/Analytics.php
+++ analytics/php/example/app/Http/Middleware/Analytics.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
  
 class Analytics {
+
     public function handle($request, Closure $next) {
         $start = microtime(true);
         $response = $next($request);
