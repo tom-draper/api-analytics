2acb49f16a321d03a42d51ca046e7d4beb723d19
Tom Draper <tomjdraper1@gmail.com>
2022-12-21 02:03:43 +0000

    Refactor

--- dashboard/src/routes/Home.svelte
+++ dashboard/src/routes/Home.svelte
@@ -9,6 +9,11 @@
   let framework = 'Django';
 </script>
 
+<svelte:head>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism.min.css" rel="stylesheet" />
+</svelte:head>
+
+
 <div class="home">
   <div class="landing-page-container">
     <div class="landing-page">
