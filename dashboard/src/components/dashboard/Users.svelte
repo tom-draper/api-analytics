da3a205d6e4cc2846e87020be2c3dfe71ac75ac8
Tom Draper <tomjdraper1@gmail.com>
2023-01-02 15:37:16 +0000

    Add users card to dashboard

--- dashboard/src/components/dashboard/Users.svelte
+++ dashboard/src/components/dashboard/Users.svelte
@@ -1,36 +1,50 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import periodToDays from "../../lib/period";
-
-  function setPercentageChange() {
-    if (prevData.length == 0) {
-      percentageChange = null;
-    } else {
-      percentageChange = (data.length / prevData.length) * 100 - 100;
-    }
-  }
-
-  function setRequestsPerHour() {
-    if (data.length > 0) {
-      let days = periodToDays(period);
-      if (days != null) {
-        requestsPerHour = (data.length / (24 * days)).toFixed(2);
-      }
-    } else {
-      requestsPerHour = "0";
-    }
-  }
   
   function togglePeriod() {
     perHour = !perHour;
   }
-  
+
+  function setPercentageChange(now: number, prev: number) {
+    if (prev == 0) {
+      percentageChange = null;
+    } else {
+      percentageChange = (now / prev) * 100 - 100;
+    }
+  }
+
+  function getUsers(data: RequestsData): Set<string> {
+    let users: Set<string> = new Set();
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].ip_address) {
+        users.add(data[i].ip_address)
+      }
+    }
+    return users
+  }
+
   function build() {
-    setPercentageChange();
-    setRequestsPerHour();
+    let users = getUsers(data);
+    numUsers = users.size;
+    
+    let prevUsers = getUsers(prevData);
+    let prevNumUsers = prevUsers.size;
+
+    setPercentageChange(numUsers, prevNumUsers);
+
+    if (numUsers > 0) {
+      let days = periodToDays(period);
+      if (days != null) {
+        usersPerHour = (numUsers/ (24 * days)).toFixed(2);
+      }
+    } else {
+      usersPerHour = "0";
+    }
   }
   
-  let requestsPerHour: string;
+  let numUsers: number = 0;
+  let usersPerHour: string;
   let perHour = false;
   let percentageChange: number;
   let mounted = false;
@@ -46,10 +60,10 @@
 {#if perHour}
   <button class="card" on:click="{togglePeriod}">
     <div class="card-title">
-      Requests <span class="per-hour">/ hour</span>
+      Users <span class="per-hour">/ hour</span>
     </div>
-    {#if requestsPerHour != undefined}
-      <div class="value">{requestsPerHour}</div>
+    {#if usersPerHour != undefined}
+      <div class="value">{usersPerHour}</div>
     {/if}
   </button>
 {:else}
@@ -63,15 +77,15 @@
         ({percentageChange > 0 ? "+" : ""}{percentageChange.toFixed(1)}%)
       </div>
     {/if}
-    <div class="card-title">Requests</div>
-    <div class="value">{data.length.toLocaleString()}</div>
+    <div class="card-title">Users</div>
+    <div class="value">{numUsers.toLocaleString()}</div>
   </button>
 {/if}
 
 <style scoped>
   .card {
     width: calc(200px - 1em);
-    margin: 0 1em 0 2em;
+    margin: 0 2em 0 1em;
     position: relative;
     cursor: pointer;
   }
