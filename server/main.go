894f947e8982f3613eb4bc011dcf382d487c5d04
Tom Draper <tomjdraper1@gmail.com>
2023-03-26 20:11:17 +0100

    fix: minor changes to server

--- server/main.go
+++ server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"server/lib"
 
 	"github.com/gin-contrib/cors"
@@ -12,12 +10,12 @@ import (
 func main() {
 	db := lib.OpenDBConnection()
 
+	gin.SetMode(gin.ReleaseMode)
 	app := gin.New()
 
 	r := app.Group("/api")
 	r.Use(cors.Default())
 	lib.RegisterRouter(r, db) // Register route
 
-	fmt.Println("https://localhost:8080")
-	app.Run(":8080")
+	app.Run(":3000")
 }
