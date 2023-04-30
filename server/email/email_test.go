bebe9522d01e3cd0790fb912e35949994b363e31
Tom Draper <tomjdraper1@gmail.com>
2023-04-30 21:19:19 +0100

    fix: Minor bugs

--- server/email/email_test.go
+++ server/email/email_test.go
@@ -7,18 +7,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func getTestEmailAddress() string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		panic(err)
-	}
-
-	address := os.Getenv("TEST_EMAIL_ADDRESS")
-	return address
-}
-
 func TestSendEmail(t *testing.T) {
-	address := getTestEmailAddress()
+	address := GetEmailAddress()
 	err := SendEmail("Test Subject", "Test Body", address)
 	if err != nil {
 		t.Error(err)
