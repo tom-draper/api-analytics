4297f51aa75b811c9b5fe7017ad2d325ed750dd2
Tom Draper <tomjdraper1@gmail.com>
2023-08-21 19:40:16 +0100

    feat: google search console verification

--- dashboard/server.js
+++ dashboard/server.js
@@ -15,6 +15,7 @@ server.get("*", function(req, res) {
       <link rel='stylesheet' href='/global.css'>
       <link rel='stylesheet' href='/bundle.css'>
       <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+      <meta name="google-site-verification" content="vW5m_ij7wc85Td5Syr9N81a1efg_TTwcN8kOiXBc5c0" />
       <title>API Analytics</title>
       <script src="https://cdn.plot.ly/plotly-latest.min.js" type="text/javascript"></script>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
