15a6f569c8d8be765d74ddaaf34b2973224db982
Tom Draper <tomjdraper1@gmail.com>
2023-09-09 14:14:43 +0100

    chore: update dashboard dependencies

--- dashboard/rollup.config.js
+++ dashboard/rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript"
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@rollup/plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import json from '@rollup/plugin-json';
 
