dbf96f58cc74563937ba312d5141630ee46dfdc2
Tom Draper <tomjdraper1@gmail.com>
2023-12-01 19:16:51 +0000

    feat: API Analytics config for Go frameworks

--- analytics/go/echo/analytics.go
+++ analytics/go/echo/analytics.go
@@ -7,17 +7,29 @@ import (
 	"github.com/tom-draper/api-analytics/analytics/go/core"
 )
 
+type Config struct {
+	GetPath      func(c echo.Context) string
+	GetHostname  func(c echo.Context) string
+	GetUserAgent func(c echo.Context) string
+	GetIPAddress func(c echo.Context) string
+	GetUserID    func(c echo.Context) string
+}
+
 func Analytics(apiKey string) echo.MiddlewareFunc {
+	return AnalyticsWithConfig(apiKey, &Config{})
+}
+
+func AnalyticsWithConfig(apiKey string, config *Config) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			start := time.Now()
 			err := next(c)
 
 			data := core.RequestData{
-				Hostname:     c.Request().Host,
-				IPAddress:    c.RealIP(),
-				Path:         c.Request().URL.Path,
-				UserAgent:    c.Request().UserAgent(),
+				Hostname:     getHostname(c, config),
+				IPAddress:    getIPAddress(c, config),
+				Path:         getPath(c, config),
+				UserAgent:    getUserAgent(c, config),
 				Method:       c.Request().Method,
 				Status:       c.Response().Status,
 				ResponseTime: time.Since(start).Milliseconds(),
@@ -28,4 +40,39 @@ func Analytics(apiKey string) echo.MiddlewareFunc {
 			return err
 		}
 	}
+}
+
+func getHostname(c echo.Context, config *Config) string {
+	if config.GetHostname != nil {
+		return config.GetHostname(c)
+	}
+	return c.Request().Host
+}
+
+func getPath(c echo.Context, config *Config) string {
+	if config.GetPath != nil {
+		return config.GetPath(c)
+	}
+	return c.Request().URL.Path
+}
+
+func getUserAgent(c echo.Context, config *Config) string {
+	if config.GetUserAgent != nil {
+		return config.GetUserAgent(c)
+	}
+	return c.Request().UserAgent()
+}
+
+func getIPAddress(c echo.Context, config *Config) string {
+	if config.GetIPAddress != nil {
+		return config.GetIPAddress(c)
+	}
+	return c.RealIP()
+}
+
+func getUserID(c echo.Context, config *Config) string {
+	if config.GetUserID != nil {
+		return config.GetUserID(c)
+	}
+	return ""
 }
