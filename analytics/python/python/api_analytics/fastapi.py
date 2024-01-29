55f2eec220e5a3d59cf3b70313770650fdb9923a
Tom Draper <tomjdraper1@gmail.com>
2024-01-29 21:04:10 +0000

    feat: custom user ID for Python middleware

--- analytics/python/python/api_analytics/fastapi.py
+++ analytics/python/python/api_analytics/fastapi.py
@@ -21,17 +21,27 @@ class Config:
     :param get_ip_address: Optional custom mapping function that takes a request
         and returns the IP address stored within the request. If set, it
         overrides the default behaviour of API Analytics.
-    :param get_hostname: Optional custom mapping function that takes a request and
-        returns the hostname stored within the request. If set, it overrides
+    :param get_hostname: Optional custom mapping function that takes a request
+        and returns the hostname stored within the request. If set, it overrides
         the default behaviour of API Analytics.
     :param get_user_agent: Optional custom mapping function that takes a request
         and returns the user agent stored within the request. If set, it
         overrides the default behaviour of API Analytics.
-    :param get_user_id: Optional custom mapping function that takes a request and
-        returns a custom user ID stored within the request. If set, this can be
-        used to track a custom user ID specific to your API such as an API key
-        or client ID. If left as `None`, no custom user ID will be used, and user
-        identification will rely on client IP address only.
+    :param get_user_id: Optional custom mapping function that takes a request
+        and returns a custom user ID stored within the request. If set, this can
+        be used to track a custom user ID specific to your API such as an API
+        key or client ID. If left as `None`, no custom user ID will be used, and
+        user identification may rely on client IP address only (depending on
+        `privacy_level`).
+    :param privacy_level: Controls client identification by IP address.
+        - 0: Sends client IP to the server to be stored and client location is
+        inferred.
+        - 1: Sends the client IP to the server only for the location to be
+        inferred and stored, with the IP discarded afterwards.
+        - 2: Avoids sending the client IP address to the server. Providing a
+        custom `get_user_id` mapping function becomes the only method for client
+        identification.
+        Defaults to 0.
     """
 
     get_path: Union[Callable[[Request], str], None] = None
@@ -39,6 +49,7 @@ class Config:
     get_hostname: Union[Callable[[Request], str], None] = None
     get_user_agent: Union[Callable[[Request], str], None] = None
     get_user_id: Union[Callable[[Request], str], None] = None
+    privacy_level: int = 0
 
 
 class Analytics(BaseHTTPMiddleware):
@@ -64,6 +75,10 @@ class Analytics(BaseHTTPMiddleware):
         return request.url.path
 
     def _get_ip_address(self, request: Request) -> Union[str, None]:
+        # If privacy_level is max, client IP address is never sent to the server
+        if self.config.privacy_level >= 2:
+            return None
+
         if self.config.get_ip_address:
             return self.config.get_ip_address(request)
         return request.client.host
@@ -92,8 +107,9 @@ class Analytics(BaseHTTPMiddleware):
             "method": request.method,
             "status": response.status_code,
             "response_time": int((time() - start) * 1000),
+            "user_id": self._get_user_id(request),
             "created_at": datetime.now().isoformat(),
         }
 
-        log_request(self.api_key, request_data, "FastAPI")
+        log_request(self.api_key, request_data, "FastAPI", self.config.privacy_level)
         return response
--- analytics/python/tornado/api_analytics/core.py
+++ analytics/python/tornado/api_analytics/core.py
@@ -8,15 +8,22 @@ _requests = []
 _last_posted = datetime.now()
 
 
-def _post_requests(api_key: str, requests_data: List[Dict], framework: str):
+def _post_requests(
+    api_key: str, requests_data: List[Dict], framework: str, privacy_level: int
+):
     requests.post(
         "https://www.apianalytics-server.com/api/log-request",
-        json={"api_key": api_key, "requests": requests_data, "framework": framework},
+        json={
+            "api_key": api_key,
+            "requests": requests_data,
+            "framework": framework,
+            "privacy_level": privacy_level,
+        },
         timeout=10,
     )
 
 
-def log_request(api_key: str, request_data: Dict, framework: str):
+def log_request(api_key: str, request_data: Dict, framework: str, privacy_level: int):
     if api_key == "" or api_key is None:
         return
     global _requests, _last_posted
@@ -24,7 +31,7 @@ def log_request(api_key: str, request_data: Dict, framework: str):
     now = datetime.now()
     if (now - _last_posted).total_seconds() > 60.0:
         threading.Thread(
-            target=_post_requests, args=(api_key, _requests, framework)
+            target=_post_requests, args=(api_key, _requests, framework, privacy_level)
         ).start()
         _requests = []
         _last_posted = now
