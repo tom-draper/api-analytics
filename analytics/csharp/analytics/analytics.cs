ead9015ed08446ad10b12200e5e704951a5ae03c
Tom Draper <tomjdraper1@gmail.com>
2024-02-18 10:52:24 +0000

    feat: config for ASP.NET Core middleware

--- analytics/csharp/analytics/analytics.cs
+++ analytics/csharp/analytics/analytics.cs
@@ -8,20 +8,24 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
 
-public class Analytics
+
+public class Analytics(RequestDelegate next, string apiKey, Config? config = null)
 {
-    private readonly RequestDelegate _next;
-    private static string? _apiKey;
-    private DateTime _lastPosted;
-    private List<RequestData> _requests;
-    private static readonly string _url = "https://www.apianalytics-server.com/api/log-request";
+    private readonly RequestDelegate _next = next;
+    private readonly string? _apiKey = apiKey;
+    private readonly Config _config = config ?? new Config();
+    private DateTime _lastPosted = DateTime.Now;
+    private List<RequestData> _requests = [];
+    private readonly string _url = "https://www.apianalytics-server.com/api/log-request";
 
     private struct Payload
     {
         [JsonPropertyName("api_key")]
         public string ApiKey { get; set; }
+
         [JsonPropertyName("requests")]
         public List<RequestData> Requests { get; set; }
+
         [JsonPropertyName("framework")]
         public string Framework { get; set; }
     }
@@ -30,20 +34,29 @@ public class Analytics
     {
         [JsonPropertyName("hostname")]
         public string Hostname { get; set; }
+
         [JsonPropertyName("ip_address")]
         public string IPAddress { get; set; }
+
         [JsonPropertyName("user_agent")]
         public string UserAgent { get; set; }
+
         [JsonPropertyName("path")]
         public string Path { get; set; }
+
         [JsonPropertyName("method")]
         public string Method { get; set; }
+
         [JsonPropertyName("response_time")]
         public int ResponseTime { get; set; }
+
         [JsonPropertyName("status")]
         public int Status { get; set; }
-        [JsonPropertyName("created_at")]
 
+        [JsonPropertyName("user_id")]
+        public string UserID { get; set; }
+
+        [JsonPropertyName("created_at")]
         public string CreatedAt { get; set; }
 
         public override readonly string ToString()
@@ -52,15 +65,7 @@ public class Analytics
         }
     }
 
-    public Analytics(RequestDelegate next, string apiKey)
-    {
-        _next = next;
-        _apiKey = apiKey;
-        _lastPosted = DateTime.Now;
-        _requests = [];
-    }
-
-    private static async Task PostRequest(string apiKey, List<RequestData> requests, string framework)
+    private async Task PostRequest(string apiKey, List<RequestData> requests, string framework)
     {
         var payload = new Payload
         {
@@ -100,16 +105,52 @@ public class Analytics
 
         var requestData = new RequestData
         {
-            Hostname = context.Request.Host.ToString(),
-            IPAddress = context.Connection.RemoteIpAddress?.ToString() ?? "",
-            UserAgent = context.Request.Headers.UserAgent.ToString(),
-            Path = context.Request.Path.ToString(),
-            Method = context.Request.Method.ToString(),
+            Hostname = GetHostname(context),
+            IPAddress = GetIPAddress(context),
+            UserAgent = GetUserAgent(context),
+            Path = GetPath(context),
+            Method = context.Request.Method,
             ResponseTime = watch.Elapsed.Milliseconds,
             Status = context.Response.StatusCode,
+            UserID = GetUserID(context),
             CreatedAt = createdAt.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss.fffK")
         };
         await LogRequest(requestData);
+    }
+
+    private string GetIPAddress(HttpContext context)
+    {
+        if (_config.GetIPAddress != null)
+            return _config.GetIPAddress.Invoke(context);
+        return context.Connection.RemoteIpAddress?.ToString() ?? "";
+    }
+
+    private string GetHostname(HttpContext context)
+    {
+        if (_config.GetHostname != null)
+            return _config.GetHostname.Invoke(context);
+        return context.Request.Host.ToString();
+    }
+
+    private string GetUserAgent(HttpContext context)
+    {
+        if (_config.GetUserAgent != null)
+            return _config.GetUserAgent.Invoke(context) ?? "";
+        return context.Request.Headers.UserAgent.ToString();
+    }
+
+    private string GetPath(HttpContext context)
+    {
+        if (_config.GetPath != null)
+            return _config.GetPath.Invoke(context);
+        return context.Request.Path.ToString();
+    }
+
+    private string GetUserID(HttpContext context)
+    {
+        if (_config.GetUserID != null)
+            return _config.GetUserID.Invoke(context);
+        return "";
     }
 }
 
@@ -119,4 +160,14 @@ public static class AnalyticsExtensions
     {
         return app.UseMiddleware<Analytics>(apiKey);
     }
+}
+
+public class Config
+{
+    public Func<HttpContext, string>? GetPath { get; set; } = null;
+    public Func<HttpContext, string>? GetIPAddress { get; set; } = null;
+    public Func<HttpContext, string>? GetHostname { get; set; } = null;
+    public Func<HttpContext, string>? GetUserAgent { get; set; } = null;
+    public Func<HttpContext, string>? GetUserID { get; set; } = null;
+    public int PrivacyLevel { get; set; } = 0;
 }
