b3bca16064afb875b02ff76e4a46e3dfe872c81b
Tom Draper <tomjdraper1@gmail.com>
2025-06-25 21:35:56 +0100

    refactor: remove user hash and preserve backwards compatibility with dashboard for referrer field addition

--- server/api/lib/routes/routes.go
+++ server/api/lib/routes/routes.go
@@ -725,7 +725,6 @@ func buildRequestData(rows pgx.Rows) []RequestData {
 				Path:         request.Path,
 				Hostname:     hostname,
 				UserAgent:    userAgent,
-				UserHash:     userHash,
 				Method:       request.Method,
 				Status:       request.Status,
 				ResponseTime: request.ResponseTime,
