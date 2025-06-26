dcb9e64d1d04de0384688604f3ba7e4851769db1
Tom Draper <tomjdraper1@gmail.com>
2025-06-26 11:45:46 +0100

    refactor: api logging and env tests

--- server/api/lib/env/env.go
+++ server/api/lib/env/env.go
@@ -6,25 +6,25 @@ import (
 	"strconv"
 
 	"github.com/joho/godotenv"
-	"github.com/tom-draper/api-analytics/server/api/lib/log"
 )
 
-func GetIntegerEnvVariable(name string, defaultValue int) int {
+// LoadEnv loads environment variables from a .env file.
+func LoadEnv() error {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.LogToFile(fmt.Sprintf("Failed to load .env file. Using default value %s=%d.", name, defaultValue))
-		return defaultValue
+		return fmt.Errorf("failed to load .env file: %w", err)
 	}
+	return nil
+}
 
+func GetIntegerEnvVariable(name string, defaultValue int) int {
 	valueStr := os.Getenv(name)
 	if valueStr == "" {
-		log.LogToFile(fmt.Sprintf("%s environment variable is blank. Using default value %s=%d.", name, name, defaultValue))
 		return defaultValue
 	}
 
 	value, err := strconv.Atoi(valueStr)
 	if err != nil {
-		log.LogToFile(fmt.Sprintf("%s environment variable is not an integer. Using default value %s=%d.", name, name, defaultValue))
 		return defaultValue
 	}
 
@@ -32,17 +32,9 @@ func GetIntegerEnvVariable(name string, defaultValue int) int {
 }
 
 func GetEnvVariable(name string, defaultValue string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.LogToFile(fmt.Sprintf("Failed to load .env file. Using default value %s=%s.", name, defaultValue))
-		return defaultValue
-	}
-
 	value := os.Getenv(name)
 	if value == "" {
-		log.LogToFile(fmt.Sprintf("%s environment variable is blank. Using default value %s=%s.", name, name, defaultValue))
 		return defaultValue
 	}
-
 	return value
 }
