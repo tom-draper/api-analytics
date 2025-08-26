31bd583f9184b6f4e629a817c60d12b1c9f9f1db
Tatayoyoh <r.dubodan@gmail.com>
2025-08-26 13:27:29 +0200

    Improve Go server/api LogToFile can now log to stdout with environement variable LOG_OUTPUT_FILE=stdout

--- server/api/lib/log/log.go
+++ server/api/lib/log/log.go
@@ -3,15 +3,25 @@ package log
 import (
 	"log"
 	"os"
+	"strings"
 )
 
 func LogToFile(msg string) {
-	f, err := os.OpenFile("./api.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+	logFile := os.Getenv("LOG_OUTPUT_FILE")
+	if logFile == "" {
+		logFile = "./api.log"
 	}
-	defer f.Close()
-
-	log.SetOutput(f)
-	log.Println(msg)
+	
+	if strings.ToLower(logFile) == "stdout" {
+		log.Println(msg)
+	} else {
+		f, err := os.OpenFile("./api.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer f.Close()
+	
+		log.SetOutput(f)
+		log.Println(msg)
+	}
 }
