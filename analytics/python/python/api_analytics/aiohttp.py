662646b9944e79ef1809c1d7e914a29ca96a1552
Tom Draper <tomjdraper1@gmail.com>
2026-03-10 15:15:26 +0000

    feat(python): add aiohttp middleware support

---
+++ analytics/python/python/api_analytics/aiohttp.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass
+from datetime import datetime
+from time import time
+from typing import Callable, Union
+
+from .core import log_request, logger, DEFAULT_SERVER_URL
+from aiohttp import web
+
+
+def add_middleware(app: web.Application, api_key: str, config: "Config" = None):
+    """
+    Adds API Analytics middleware to the aiohttp app to log requests to the server.
+
+    :param app: aiohttp Application to attach the analytics middleware to
+    :param api_key: API key for API Analytics
+    :param config: Optional configuration for the middleware
+    """
+    config = config or Config()
+
+    if not api_key:
+        logger.debug("API key is not set.")
+    if not config.server_url:
+        logger.debug("Server URL is not set.")
+
+    @web.middleware
+    async def analytics_middleware(request: web.Request, handler) -> web.Response:
+        start = time()
+        response = await handler(request)
+
+        request_data = {
+            "hostname": config.get_hostname(request),
+            "ip_address": _get_ip_address(request, config),
+            "path": config.get_path(request),
+            "user_agent": config.get_user_agent(request),
+            "method": request.method,
+            "status": response.status,
+            "response_time": int((time() - start) * 1000),
+            "user_id": config.get_user_id(request),
+            "created_at": datetime.now().isoformat(),
+        }
+
+        log_request(
+            api_key, request_data, "aiohttp", config.privacy_level, config.server_url
+        )
+        return response
+
+    app.middlewares.insert(0, analytics_middleware)
+
+
+class Mappers:
+    @staticmethod
+    def get_path(request: web.Request) -> Union[str, None]:
+        return request.path
+
+    @staticmethod
+    def get_ip_address(request: web.Request) -> Union[str, None]:
+        return request.remote
+
+    @staticmethod
+    def get_hostname(request: web.Request) -> Union[str, None]:
+        return request.host
+
+    @staticmethod
+    def get_user_id(request: web.Request) -> Union[str, None]:
+        return None
+
+    @staticmethod
+    def get_user_agent(request: web.Request) -> Union[str, None]:
+        return request.headers.get("User-Agent")
+
+
+def _get_ip_address(request: web.Request, config: "Config") -> Union[str, None]:
+    if config.privacy_level >= 2:
+        return None
+    return config.get_ip_address(request)
+
+
+@dataclass
+class Config:
+    """
+    Configuration for the aiohttp API Analytics middleware.
+
+    :param privacy_level: Controls client identification by IP address.
+        - 0: Sends client IP to the server to be stored and client location is
+        inferred.
+        - 1: Sends the client IP to the server only for the location to be
+        inferred and stored, with the IP discarded afterwards.
+        - 2: Avoids sending the client IP address to the server. Providing a
+        custom `get_user_id` mapping function becomes the only method for client
+        identification.
+        Defaults to 0.
+    :param server_url: For self-hosting. Points to the public server url to post
+        requests to.
+    :param get_path: Mapping function that takes a request and returns the path
+        stored within the request. Assigning a value will override the default
+        behavior.
+    :param get_ip_address: Mapping function that takes a request and returns the
+        IP address stored within the request. Assigning a value will override the
+        default behavior.
+    :param get_hostname: Mapping function that takes a request and returns the
+        hostname stored within the request. Assigning a value will override the
+        default behavior.
+    :param get_user_agent: Mapping function that takes a request and returns the
+        user agent stored within the request. Assigning a value will override the
+        default behavior.
+    :param get_user_id: Mapping function that takes a request and returns a
+        custom user ID stored within the request. Always returns None by default.
+        Assigning a value allows for tracking a custom user ID specific to your API
+        such as an API key or client ID. If left as the default value, user
+        identification may rely on client IP address only (depending on
+        `privacy_level`).
+    """
+
+    privacy_level: int = 0
+    server_url: str = DEFAULT_SERVER_URL
+    get_path: Callable[[web.Request], Union[str, None]] = Mappers.get_path
+    get_ip_address: Callable[[web.Request], Union[str, None]] = Mappers.get_ip_address
+    get_hostname: Callable[[web.Request], Union[str, None]] = Mappers.get_hostname
+    get_user_agent: Callable[[web.Request], Union[str, None]] = Mappers.get_user_agent
+    get_user_id: Callable[[web.Request], Union[str, None]] = Mappers.get_user_id
