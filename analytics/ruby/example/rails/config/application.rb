9dbd7553489d14a34f95f7fbc3f50471aafb49b4
Tom Draper <tomjdraper1@gmail.com>
2026-03-10 14:17:55 +0000

    refactor: minimal Rails example, fix Sinatra require, add RSpec tests

--- analytics/ruby/example/rails/config/application.rb
+++ analytics/ruby/example/rails/config/application.rb
@@ -1,28 +1,11 @@
-require_relative "boot"
+require_relative 'boot'
+require 'rails/all'
 
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "rails/test_unit/railtie"
-require 'dotenv'
-
-require "api_analytics"
-
-Dotenv.load()
 Bundler.require(*Rails.groups)
 
-
-module RailsMiddleware
+module RailsExample
   class Application < Rails::Application
-    config.load_defaults 6.1
+    config.load_defaults 7.1
     config.api_only = true
 
     config.middleware.use ::Analytics::Rails, ENV['API_KEY']  # Add middleware
