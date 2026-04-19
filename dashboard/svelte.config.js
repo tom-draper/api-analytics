7e542f5d600c3d94f9216594179c2596a2977806
Tom Draper <tomjdraper1@gmail.com>
2026-04-19 10:18:27 +0100

    chore: switch SvelteKit adapter from node to auto

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,11 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			out: 'build',
-			precompress: true,
-			envPrefix: ''
-		}),
+		adapter: adapter(),
 		alias: {
 			"$components": "./src/lib/components",
 			"$components/*": "./src/lib/components/*"
